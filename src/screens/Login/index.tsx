050caeaa2ea2407cfbbe06ce01d704dd198eb6e4
Maksym Bocharov <Maksym Bocharov>
2024-06-08 15:10:37 +0300

    Implement initial folders

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const LoginScreen = () => {
+ return (
+   <SafeAreaView>
+     <Text>тут верстка самого екрана, который включает в себя переиспользуемые компоненты</Text>
+   </SafeAreaView>
+ )
+}
+
+export default LoginScreen
